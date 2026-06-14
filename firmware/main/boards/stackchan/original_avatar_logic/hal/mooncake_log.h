c36be4e109f38d73d574f11809d738c8c1a8f933
luyi-he <228930812@qq.com>
2026-06-14 12:14:36 +0800

    Fix build: Enhance mock mooncake_log.h to support tagError and underscored variants

--- firmware/main/boards/stackchan/original_avatar_logic/hal/mooncake_log.h
+++ firmware/main/boards/stackchan/original_avatar_logic/hal/mooncake_log.h
@@ -1,12 +1,32 @@
 /*
- * Mock mooncake_log.h
+ * Mock mooncake_log.h for StackChan Integration
  */
 #pragma once
 #include <esp_log.h>
 #include <string_view>
 
 namespace mclog {
-    inline void tagInfo(const std::string_view& tag, const char* format, ...) {
-        // Just use ESP_LOGI
+    // Basic mock that accepts variadic arguments
+    template<typename... Args>
+    inline void tagInfo(const char* tag, const char* format, Args... args) {
+        // Implementation can be empty or bridge to ESP_LOGI
     }
+
+    template<typename... Args>
+    inline void tagWarn(const char* tag, const char* format, Args... args) {
+    }
+
+    template<typename... Args>
+    inline void tagError(const char* tag, const char* format, Args... args) {
+    }
+
+    // Support underscored versions if needed by some files
+    template<typename... Args>
+    inline void tag_info(const char* tag, const char* format, Args... args) {}
+    
+    template<typename... Args>
+    inline void tag_warn(const char* tag, const char* format, Args... args) {}
+    
+    template<typename... Args>
+    inline void tag_error(const char* tag, const char* format, Args... args) {}
 }
